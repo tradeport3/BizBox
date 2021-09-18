b5febb03340949ed419912b70c0beda9777e2dec
mkovachev <mkovachev6@gmail.com>
2021-09-18 16:57:29 +0300

    .

--- Domain/Models/Interview.cs
+++ Domain/Models/Interview.cs
@@ -2,5 +2,14 @@
 {
     public class Interview
     {
+        private readonly HashSet<string> questions;
+
+        public Interview()
+        {
+            this.questions = new HashSet<string>();
+        }
+        public IReadOnlyCollection<string> Questions => this.questions.ToList().AsReadOnly();
+
+        public void AddQuestion(string question) => this.questions.Add(question);
     }
 }
