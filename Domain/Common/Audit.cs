2125d16da674f7c77b904de916e89185bdf4e095
mkovachev <mkovachev6@gmail.com>
2021-09-18 16:16:38 +0300

    .

--- Domain/Common/Audit.cs
+++ Domain/Common/Audit.cs
@@ -1,26 +1,14 @@
-﻿using Domain.Exceptions;
-
-namespace Domain.Common
+﻿namespace Domain.Common
 {
     public abstract class Audit : IAudit
     {
-        private string createdBy;
-        private string modifiedBy;
-
-        public string CreatedBy
-        {
-            get => this.createdBy;
-            set => this.createdBy = value ?? throw new BaseException("User Id cannot be null.");
-        }
-
         public DateTime CreatedOn { get; set; }
 
-        public string ModifiedBy
-        {
-            get => this.modifiedBy;
-            set => this.modifiedBy = value ?? throw new BaseException("User Id cannot be null.");
-        }
-
         public DateTime? ModifiedOn { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public string ModifiedBy { get; set; }
+
     }
 }
--- Domain/Common/IAudit.cs
+++ Domain/Common/IAudit.cs
@@ -2,12 +2,12 @@
 {
     public interface IAudit
     {
-        public string CreatedBy { get; set; }
-
         public DateTime CreatedOn { get; set; }
 
-        public string ModifiedBy { get; set; }
-
         public DateTime? ModifiedOn { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public string ModifiedBy { get; set; }
     }
 }
