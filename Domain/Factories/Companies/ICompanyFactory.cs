95fc751ba7f03802658cf2fca7b5988a29c883d8
mkovachev <mkovachev6@gmail.com>
2021-09-18 22:15:53 +0300

    .

---
+++ Domain/Factories/Companies/ICompanyFactory.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Common;
+using Domain.Models;
+
+namespace Domain.Factories.Companies
+{
+    public interface ICompanyFactory : IFactory<Company>
+    {
+        ICompanyFactory WithName(string name);
+    }
+}
