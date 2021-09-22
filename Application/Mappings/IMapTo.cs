338cbf4e0f8c80d44258e1b7a96730dd528463d8
mkovachev <mkovachev6@gmail.com>
2021-09-22 09:43:29 +0300

    .

---
+++ Application/Mappings/IMapTo.cs
@@ -0,0 +1,9 @@
+﻿using AutoMapper;
+
+namespace Application.Mappings
+{
+    public interface IMapTo<T>
+    {
+        void Mapping(Profile mapper) => mapper.CreateMap(this.GetType(), typeof(T));
+    }
+}
