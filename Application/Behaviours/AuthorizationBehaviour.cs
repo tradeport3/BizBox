6576445694405a55e461eab7e7e2f0d5b4d6b6cd
mkovachev <mkovachev6@gmail.com>
2021-09-22 09:11:01 +0300

    .

---
+++ Application/Behaviours/AuthorizationBehaviour.cs
@@ -0,0 +1,82 @@
+﻿using System.Reflection;
+using Application.Exceptions;
+using Application.Identity;
+using Application.Interfaces;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Application.Behaviours
+{
+    public class AuthorizationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ICurrentUser currentUser;
+        private readonly IIdentity identity;
+
+        public AuthorizationBehaviour(
+            ICurrentUser currentUserService,
+            IIdentity identityService)
+        {
+            currentUser = currentUserService;
+            this.identity = identityService;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            var authorizeAttributes = request.GetType().GetCustomAttributes<AuthorizeAttribute>();
+
+            if (authorizeAttributes.Any())
+            {
+                // Must be authenticated user
+                if (currentUser.Id == null)
+                {
+                    throw new UnauthorizedAccessException();
+                }
+
+                // Role-based authorization
+                var authorizeAttributesWithRoles = authorizeAttributes.Where(a => !string.IsNullOrWhiteSpace(a.Roles));
+
+                if (authorizeAttributesWithRoles.Any())
+                {
+                    var authorized = false;
+
+                    foreach (var roles in authorizeAttributesWithRoles.Select(a => a.Roles.Split(',')))
+                    {
+                        foreach (var role in roles)
+                        {
+                            var isInRole = await identity.IsInRole(currentUser.Id, role.Trim());
+                            if (isInRole)
+                            {
+                                authorized = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    // Must be a member of at least one role in roles
+                    if (!authorized)
+                    {
+                        throw new ForbiddenAccessException();
+                    }
+                }
+
+                // Policy-based authorization
+                var authorizeAttributesWithPolicies = authorizeAttributes.Where(a => !string.IsNullOrWhiteSpace(a.Policy));
+                if (authorizeAttributesWithPolicies.Any())
+                {
+                    foreach (var policy in authorizeAttributesWithPolicies.Select(a => a.Policy))
+                    {
+                        var authorized = await identity.Authorize(currentUser.Id, policy);
+
+                        if (!authorized)
+                        {
+                            throw new ForbiddenAccessException();
+                        }
+                    }
+                }
+            }
+
+            // User is authorized / authorization not required
+            return await next();
+        }
+    }
+}
---
+++ Application/Exceptions/ForbiddenAccessException.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Exceptions
+{
+    public class ForbiddenAccessException : Exception
+    {
+        public ForbiddenAccessException() : base() { }
+    }
+}
