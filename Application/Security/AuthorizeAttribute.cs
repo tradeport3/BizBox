85da4de3ebeaeb3df7a182981a65e2a842fcaa0a
mkovachev <mkovachev6@gmail.com>
2021-09-22 09:37:41 +0300

    .

---
+++ Application/Security/AuthorizeAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Security
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
+    public class AuthorizeAttribute : Attribute
+    {
+        public AuthorizeAttribute() { }
+
+        public string? Roles { get; set; }
+
+        public string? Policy { get; set; }
+    }
+}
